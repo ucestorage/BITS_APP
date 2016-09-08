4e5729c9b4ea5c9e9dc348cf3ea3307a7492a39d
ucestorage <ubboeicke@gmail.com>
2016-09-08 12:15:58 +0200

    code kommentiert und formatiert

--- app/src/main/java/uce/bits_app/rss_adapter.java
+++ app/src/main/java/uce/bits_app/rss_adapter.java
@@ -10,32 +10,36 @@ import java.util.List;
 
 /**
  * Created by Ubbo on 25.08.2016.
+ * Wurde anhand eines Online-Tutorials erstellt,
+ * es wurden einige Anpassungen vorgenommen damit das ganze in gewünschter Weise funktioniert
  */
 public class rss_adapter extends BaseAdapter {
 
-    private  List<rss_Item> items;
-    private  Context context;
+    private List<rss_Item> items;
+    private Context context;
 
     public rss_adapter(Context context, List<rss_Item> items) {
         this.items = items;
         this.context = context;
     }
-
+    //Wieviel Items gibt es
     @Override
     public int getCount() {
         return items.size();
     }
-
+    //Items werde nabgeholt
     @Override
     public Object getItem(int position) {
         return items.get(position);
     }
-
+    //item ID wird abgefragt
     @Override
     public long getItemId(int id) {
         return id;
     }
-
+    //Das Layout des rss_items wird abgefragt,
+    // es wird ein ViewHolder erstellt,
+    // der die Rss-item-titel darstellt
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
         ViewHolder holder;
--- app/src/main/java/uce/bits_app/rss_constants.java
+++
@@ -1,9 +0,0 @@
-package uce.bits_app;
-
-/**
- * Created by Ubbo on 25.08.2016.
- */
-public class rss_constants {
-    public static final String TAG = "RssApp";
-
-}
